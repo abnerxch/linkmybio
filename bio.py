9f2a8c250fa676aba5802d93ea22e457e34959ba
abnerxch <axocop@ufm.edu>
2019-10-06 10:22:34 -0600

    Actualizando el yaml para que pueda ser leído correctamente

--- bio.py
+++ bio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import yaml, optparse
 import os
 
@@ -7,20 +7,33 @@ environment = os.getenv("ENVIRONMENT", "development")
 
 app = Flask(__name__)
 
-info = yaml.load(open('links.yaml', 'r'))
+class SocialMedia():
+    def __init__(self, name, enable, link, description):
+        self.name = name
+        self.enable = enable
+        self.link = link
+        self.description = description
 
+listSocialLinks = []
+with open("links.yaml", 'r') as stream:
+    try:
+        links = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+linkList = links.get("links")
 
-links = []
-for i in range(len(info['links'])):
-    x = info['links'][i].get(i+1).get('link')
-    links.append(x)
+for i in range(len(linkList)):
+    name = linkList[i].get(i+1).get("name")
+    enable = linkList[i].get(i+1).get("enable")
+    link = linkList[i].get(i+1).get("link")
+    description = linkList[i].get(i+1).get("description")
 
-print(links[0])
-
+    social_media = SocialMedia(name, enable, link, description)
+    listSocialLinks.append(social_media)
 
 @app.route('/')
 def index():
-    return render_template("index.html", name=info['name'], image=info['picture'], description=info['shortbio'], link=links)
+    return render_template("index.html", social_media_list=listSocialLinks, links=links)
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
